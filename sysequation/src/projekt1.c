8fa09f73de8bd1f1ba167e1d841cf8c91dc8725c
Andreas Bachmann <bachi@te-clan.ch>
2016-12-09 11:55:18 +0100

    comment-in add functions

--- sysequation/src/projekt1.c
+++ sysequation/src/projekt1.c
@@ -7,12 +7,11 @@ main(int argc, const char *argv[])
     main_permutation();
     main_single();
     main_single_scale();
-    /*
     main_multi();
     main_lu();
     main_hilbert();
     main_inv();
-    */
+
     return 0;
     
 }
